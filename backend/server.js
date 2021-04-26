82f0fe176778278422bd5aac10c0a2359bebb050
Rishabh7355 <rishabhbaranwal4202gmail.com>
2021-04-27 00:28:33 +0530

    folder structure changes

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,7 @@ app.use('/api/users', users)
 
 // Deploying on server ******************
 const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')))
